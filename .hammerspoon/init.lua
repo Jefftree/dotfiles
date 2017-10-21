bcb918b74305d02b76936a71599729e1b74ee07f
Jefftree <jeffrey.ying86@live.com>
2017-10-21 10:25:32 -0400

    fix lua indentation

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -21,30 +21,32 @@ local snapleft = {x = 0, y = 0, w = gwidth/2, h = gheight}
 local snapright = {x = gwidth/2, y = 0, w = gwidth/2, h = gheight}
 local snapfull = {x = 0, y = 0, w = gwidth, h = gheight}
 
-local scrs = screen.allScreens()
-local sPrimary = scrs[1]
-local sSecondary = scrs[2]
-
-local dualPane = {
-   Airmail = {1, snapfull},
-   iTerm2 = {2, snapright},
-   ["Path Finder"] = {2, snapleft},
-   Emacs = {1, snapleft},
-   Firefox = {1, snapright},
+local singlePane = {
+  Airmail = {1, snapfull},
+  iTerm2 = {1, snapfull},
+  ["Path Finder"] = {1, snapfull},
+  Emacs = {1, snapfull},
+  Firefox = {1, snapfull},
 }
 
-local configureLayout = function()
-   for app, location in pairs(dualPane) do
-      local app = appfinder.appFromName(app)
-      if app then
-         local scrs = screen.allScreens()
-         local win = app:mainWindow()
-         grid.set(win, location[2], scrs[location[1]])
-      end
-   end
-end
+local dualPane = {
+  Airmail = {1, snapfull},
+  iTerm2 = {2, snapright},
+  ["Path Finder"] = {2, snapleft},
+  Emacs = {1, snapleft},
+  Firefox = {1, snapright},
+}
 
-configureLayout()
+local configureLayout = function(layout)
+  for app, location in pairs(layout) do
+    local app = appfinder.appFromName(app)
+    if app then
+      local scrs = screen.allScreens()
+      local win = app:mainWindow()
+      grid.set(win, location[2], scrs[location[1]])
+    end
+  end
+end
 
 local snapwindow = function(param)
   return function()
@@ -54,12 +56,13 @@ local snapwindow = function(param)
 end
 
 local launch = function(prog)
-   return function()
-      local app = appfinder.appFromName(prog)
-      if app then
-         app:activate()
-      end
-   end
+  return function()
+    hs.application.launchOrFocus(prog)
+    local app = appfinder.appFromName(prog)
+    if app then
+      app:activate()
+    end
+  end
 end
 
 -- Prefix hints with first char of Application
@@ -67,43 +70,46 @@ hints.showTitleThresh = 0
 hints.style = "vimperator"
 
 local bindings = {
-   Left = snapwindow(snapleft),
-   Right = snapwindow(snapright),
-   T = snapwindow(snapfull),
-   M = launch("Airmail"),
-   B = launch("Firefox"),
-   E = launch("Emacs"),
-   I = launch("Audirvana Plus"),
-   T = launch("iTerm2"),
-   F = launch("Path Finder"),
-   K = launch("kt"),
-   O = function() hints.windowHints(window.visibleWindows()) end,
+  Left = snapwindow(snapleft),
+  Right = snapwindow(snapright),
+  T = snapwindow(snapfull),
+  M = launch("Airmail"),
+  B = launch("Firefox"),
+  V = launch("mpv"),
+  E = launch("Emacs"),
+  I = launch("Audirvana Plus"),
+  I = launch("iTerm2"),
+  F = launch("Path Finder"),
+  K = launch("kt"),
+  O = function() hints.windowHints(window.visibleWindows()) end,
 }
 
 for key, func in pairs(bindings) do
-   hotkey.bind({"cmd", "shift"}, key, func)
+  hotkey.bind({"cmd", "shift"}, key, func)
 end
 
 screen.watcher.new(function()
-  if #(screen.allScreens()) == 1 then
+    if #(screen.allScreens()) == 1 then
       alert.show("Class Time")
-  elseif #(screen.allScreens()) == 2 then
+      configureLayout(singlePane)
+    elseif #(screen.allScreens()) == 2 then
       alert.show("Welcome Home")
-  end
+      -- configureLayout(dualPane)
+    end
 end):start()
 
 -- Reloading
 
 function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
+  doReload = false
+  for _,file in pairs(files) do
+    if file:sub(-4) == ".lua" then
+      doReload = true
     end
-    if doReload then
-        hs.reload()
-    end
+  end
+  if doReload then
+    hs.reload()
+  end
 end
 
 myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
@@ -113,18 +119,18 @@ alert.show("Config loaded")
 
 caffeine = hs.menubar.new()
 function setCaffeineDisplay(state)
-    if state then
-        caffeine:setTitle("AWAKE")
-    else
-        caffeine:setTitle("SLEEPY")
-    end
+  if state then
+    caffeine:setTitle("LIT")
+  else
+    caffeine:setTitle("DIM")
+  end
 end
 
 function caffeineClicked()
-    setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
+  setCaffeineDisplay(hs.caffeinate.toggle("displayIdle"))
 end
 
 if caffeine then
-    caffeine:setClickCallback(caffeineClicked)
-    setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
+  caffeine:setClickCallback(caffeineClicked)
+  setCaffeineDisplay(hs.caffeinate.get("displayIdle"))
 end
